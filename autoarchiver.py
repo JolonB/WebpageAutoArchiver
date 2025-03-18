3dab6ff8f70d3bc0af2f44dcf29d766f1e5ab867
Jolon Behrent <jolon.behrent@gmail.com>
2025-03-18 21:06:01 +1300

    Minor improvements
    Tidy up code. Reduce logging. Add extra argument

--- autoarchiver.py
+++ autoarchiver.py
@@ -12,25 +12,38 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger("autoarchiver")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("url")
-parser.add_argument("backup_file")
+parser.add_argument(
+    "url", help="The URL (including https://) of the webpage to archive"
+)
+parser.add_argument(
+    "backup_file", help="The file to track the previous version of the webpage in"
+)
+parser.add_argument(
+    "--skip-first",
+    action="store_true",
+    help="If backup_file doesn't exist yet, don't archive the page. This can be useful if you recently manually archived it.",
+)
+
 
 def backup_exists(backup_file: str) -> bool:
     logger.debug("Running backup_exists")
     return os.path.isfile(backup_file)
 
+
 def get_backup_text(backup_file: str) -> str:
-    logger.info("Running get_backup_text")
+    logger.debug("Running get_backup_text")
     with open(backup_file) as file:
         return file.read()
 
+
 def save_backup(data: str, backup_file: str):
-    logger.info("Running save_backup")
+    logger.debug("Running save_backup")
     with open(backup_file, "w") as file:
         file.write(data)
 
+
 def get_webpage_text(url: str) -> str:
-    logger.info("Running get_webpage_text")
+    logger.debug("Running get_webpage_text")
     try:
         site_data = requests.get(url)
     except requests.exceptions.MissingSchema as e:
@@ -39,12 +52,15 @@ def get_webpage_text(url: str) -> str:
     if site_data.status_code != 200:
         logger.error("Invalid response from webpage ({})".format(site_data.status_code))
         exit(1)
-    site_contents = BeautifulSoup(site_data.text, 'html.parser').get_text()
+    site_contents = BeautifulSoup(site_data.text, "html.parser").get_text()
     return site_contents
 
+
 def save_webpage(url: str):
-    logger.info("Running save_webpage")
-    user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+    logger.debug("Running save_webpage")
+    user_agent = (
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+    )
     save_api = WaybackMachineSaveAPI(url, user_agent)
     try:
         save_api.save()
@@ -53,21 +69,25 @@ def save_webpage(url: str):
         exit(1)
     logger.info("Webpage saved and viewable at {}".format(save_api.saved_archive))
 
-def autoarchive(url: str, backup_file: str):
+
+def autoarchive(url: str, backup_file: str, skip_first: bool = False):
     current_content = get_webpage_text(url)
     previous_content = ""
     if backup_exists(backup_file):
         previous_content = get_backup_text(backup_file)
-    
-    if current_content != previous_content:
-        save_webpage(url)
-    else:
-        logger.info("Website hasn't changed")
+
+    if skip_first:
+        logger.info("No backup file found. Not archiving")
+    else: 
+        if current_content != previous_content:
+            logger.info("Archiving webpage. Please wait")
+            save_webpage(url)
+        else:
+            logger.info("Website hasn't changed. Not archiving")
 
     save_backup(current_content, backup_file)
 
 
 if __name__ == "__main__":
-    logger.info("Started")
     args = parser.parse_args()
     autoarchive(args.url, args.backup_file)
